c7d1d178f0997c28f13897c7a6d34a92494bdc98
L Carver <wifihum@gmail.com>
2023-09-12 21:07:40 -0700

    clean up comments

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -11,25 +11,28 @@ using namespace std;
 //------------------------------------------------------------------------------
 int main()
 {
-	// numbers 1 - 100
+	// numbers range from 1 - 100
 	int x = 1;
+
+	if (x % 15 == 0)
+		cout << "fizzbuzz";
 
 	// each number from 1-100
 	while (x <= 100)
 	{
-		//  divisible by 3 and by 5: display "fizzbuzz"
+		// divisible by 3 and by 5: display "fizzbuzz"
 		if (x % 15 == 0)
 			cout << "fizzbuzz";
 
-		//  divisible by 3: display "fizz"
+		// divisible by 3: display "fizz"
 		else if (x % 3 == 0)
 			cout << "fizz";
 
-		//  divisible by 5: display "buzz"
+		// divisible by 5: display "buzz"
 		else if (x % 5 == 0)
 			cout << "buzz";
 
-		//  otherwise display the number
+		// otherwise display the number
 		else
 			cout << x;
 
@@ -37,16 +40,5 @@ int main()
 
 		x = x + 1;
 	}
-	cout << "Hello World!\n";
+	cout << "Goodbye!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
