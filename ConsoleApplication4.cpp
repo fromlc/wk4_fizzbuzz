773bf1be1f66927ddcaae6341e32022780d6bb69
L Carver <wifihum@gmail.com>
2023-09-12 21:11:04 -0700

    add constants FIZZ and BUZZ, reorder ifs

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -7,6 +7,12 @@
 using namespace std;
 
 //------------------------------------------------------------------------------
+// constants
+//------------------------------------------------------------------------------
+constexpr int FIZZ = 3;
+constexpr int BUZZ = 5;
+
+//------------------------------------------------------------------------------
 // entry point
 //------------------------------------------------------------------------------
 int main()
@@ -14,22 +20,19 @@ int main()
 	// numbers range from 1 - 100
 	int x = 1;
 
-	if (x % 15 == 0)
-		cout << "fizzbuzz";
-
 	// each number from 1-100
 	while (x <= 100)
 	{
 		// divisible by 3 and by 5: display "fizzbuzz"
-		if (x % 15 == 0)
+		if (x % FIZZ == 0 && x % BUZZ == 0)
 			cout << "fizzbuzz";
 
 		// divisible by 3: display "fizz"
-		else if (x % 3 == 0)
+		else if (x % FIZZ == 0)
 			cout << "fizz";
 
 		// divisible by 5: display "buzz"
-		else if (x % 5 == 0)
+		else if (x % BUZZ == 0)
 			cout << "buzz";
 
 		// otherwise display the number
